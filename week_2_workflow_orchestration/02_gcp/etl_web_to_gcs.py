278d9564aaf839df214920d13480e78d7d2a6983
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2023-01-31 11:15:59 +0100

    Clean data after fetching

--- week_2_workflow_orchestration/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/02_gcp/etl_web_to_gcs.py
@@ -4,11 +4,24 @@ from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 from prefect.tasks import task_input_hash
 from datetime import timedelta
+from random import randint
 
 @task(cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1), retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read data from web into pandas DataFrame"""
+    # if randint(0,1) > 0:
+    #     raise Exception
     df = pd.read_csv(dataset_url)
+    return df
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Fix dtype issues"""
+    df['tpep_pickup_datetime'] =pd.to_datetime(df['tpep_pickup_datetime'])
+    df['tpep_dropoff_datetime'] =pd.to_datetime(df['tpep_dropoff_datetime'])
+    print(df.head(2))
+    print(f'columns: {df.shape[1]}')
+    print(f'rows: {df.shape[0]}')
     return df
 
 
@@ -22,6 +35,7 @@ def etl_web_to_gcs() -> None:
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{datafile}.csv.gz"
 
     df = fetch(dataset_url)
+    df_clean = clean(df)
     
 if __name__=='__main__':
     etl_web_to_gcs()
